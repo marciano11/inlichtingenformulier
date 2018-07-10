a05eec3b20e5bed78cf2ba939d5c8ac24bda6ae8
marciano11 <22320@ma-web.nl>
2018-07-10 13:36:45 +0200

    update herkansing 10-7-2018

--- site/aanvraag.php
+++ site/aanvraag.php
@@ -1,6 +1,11 @@
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="aanmelden, inlichting, formulier, inlichtingenformulier">
+<meta name="description" content="hier kun je je aanmelden voor MA inlichtingenformulier">
+<meta name="copyright" content="Dit is een eigen gemaakte inlichtingenformulier voor MA">
+<meta name="title" content="MA inlichtingenformulier">
   <head>
+      <title>MA inlichtingenformulier</title>
       <link href="style.css" rel="stylesheet" type="text/css">
 <script>
 function getUniqueRequest()
@@ -18,7 +23,8 @@ function getUniqueRequest()
       <img src="ma.jpg">
   </div>
 <div class="center">
-<h1>Aanmelden voor inlichtingenformulier</h1>
+
+<h1>MA inlichtingenformulier</h1>
 <!--Click <a href='javascript:getUniqueRequest()'>hier</a> om u aan te melden.-->
       <a href='javascript:getUniqueRequest() '><button type="submit">Aanmeld formulier</button></a>
 </div>
